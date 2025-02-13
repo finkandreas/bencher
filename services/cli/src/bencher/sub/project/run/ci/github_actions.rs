ff2f6a0cdb3d30e02d3b1428da4c9797ecf3cfb4
Andreas Fink <finkandreas@web.de>
2025-02-13 15:53:08 +0100

    explicitly set base_uri

--- services/cli/src/bencher/sub/project/run/ci/github_actions.rs
+++ services/cli/src/bencher/sub/project/run/ci/github_actions.rs
@@ -287,7 +287,7 @@ impl GitHubActions {
         let (owner, repo) = split_full_name(full_name)?;
 
         let github_client = Octocrab::builder()
-            .base_uri("https://api.github.com")
+            .base_uri("https://api.github.com").unwrap()
             .personal_token(self.token.clone())
             .build()
             .map_err(GitHubError::Auth)?;
